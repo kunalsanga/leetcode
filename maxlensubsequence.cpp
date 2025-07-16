4c9b06e1b654e22d058ed40e6cc7dfade57f9bab
kunalsanga <kunalsanga197@gmail.com>
2025-07-16 11:17:32 +0530

    add

---
+++ maxlensubsequence.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution{
+public:
+    static int maxlen(vector<int>& nums){
+        const int n = nums.size();
+        if(n==2) return 2;
+
+        bool z = nums[0] & 1;
+        int len[3] = {!z,z,1};
+
+        for (int i=1; i<n; i++){
+            bool x = nums[i] & 1;
+
+            len[x & 1]++;
+
+            if(x!=z){
+                len[2]++;
+                z=!z;
+            }
+        }
+        return max({len[0],len[1],len[2]});
+
+    }
+};
+
+int main(){
+    vector<int> nums = {2, 4, 6, 1, 3, 5, 2, 4};
+    int result = Solution::maxlen(nums);
+    cout << "Maximum Length = " << result << endl;
+    return 0;
+}
