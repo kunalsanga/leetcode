9db76583b9ed218aeefb0fbb0c67a5352d3d4ec9
kunalsanga <kunalsanga197@gmail.com>
2026-01-02 22:16:13 +0530

    n-repeated elements array

---
+++ N_repeated_array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (i + 1 < nums.length && nums[i] == nums[i + 1]) return nums[i];
+            if (i + 2 < nums.length && nums[i] == nums[i + 2]) return nums[i];
+            if (i + 3 < nums.length && nums[i] == nums[i + 3]) return nums[i];
+        }
+        return -1; // never reached
+    }
+}
