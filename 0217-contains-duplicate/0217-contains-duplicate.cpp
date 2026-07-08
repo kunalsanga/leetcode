c47360fea2440ddd52a7064dadeb05b4951acbb0
kunal sanga <152892694+kunalsanga@users.noreply.github.com>
2026-07-08 12:06:46 +0530

    Time: 77 ms (16.13%), Space: 111.2 MB (11.92%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,18 @@
+/** Approach : Using Set **/
+// Time Complexity: O(n)
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        // Create a set...
+        unordered_set<int> hset;
+        // Traverse all the elements through the loop...
+        for(int idx = 0; idx < nums.size(); idx++) {
+            // Searches set. if present, it contains duplicate...
+            if(hset.count(nums[idx]))
+                return true;
+            // insert nums[i] in set...
+            hset.insert(nums[idx]);
+        }
+        return false;
+    }
+};
